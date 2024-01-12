7fc47baac8cb0e9392ce2b98b4780889b9f86fcc
2201408SAITO <2201408@s.asojuku.ac.jp>
2024-01-12 12:07:01 +0900

    Update db-connect.php

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,8 +1,8 @@
 <?php
-const SERVER = 'mysql219.phy.lolipop.lan';
-const DBNAME = 'LAA1517364-teama';
-const USER = 'LAA1517364';
-const PASS = 'teama';
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517345-final';
+const USER = 'LAA1517345';
+const PASS = 'PASS0316';
 $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
 $pdo = new PDO($connect, USER, PASS);
 ?>
